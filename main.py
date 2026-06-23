4632ecaccbc39548a2bf44a33e8c3f49fb629fc8
elhombrelluvia <samuelasdasd123@hotmail.com>
2026-06-23 12:07:45 -0300

    Commit inicial

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask,jsonify
+from requests import Response
+
+# Initialize the Flask application
+app = Flask(__name__)
+
+# Define the route for the home page
+@app.route('/')
+def home():
+    try:
+        return jsonify({"status": "ok","code":200})
+    except Exception as e:
+        return jsonify({"status":e,"code":Response.status_code}) 
+# Run the local development server
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ test_main.py
@@ -0,0 +1,22 @@
+import pytest
+from main import app
+
+@pytest.fixture
+def client():
+    app.config["TESTING"] = True
+
+    with app.test_client() as client:
+        yield client
+
+def test_home_endpoint(client):
+    response = client.get("/")
+
+    assert response.status_code == 200
+    assert response.is_json
+
+    data = response.get_json()
+
+    assert data == {
+        "status": "ok",
+        "code": 200
+    }
